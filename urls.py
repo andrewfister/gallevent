341189ca44708828901c5777b1766499f9db9ae5
Andrew Fister <temposs@gmail.com>
2012-03-10 19:21:35 -0800

    got a page to show

--- urls.py
+++ urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
+from gallevent.map.views import show
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
     # (r'^gallevent/', include('gallevent.foo.urls')),
-
+    (r'^map/', show),
+    
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
