92ba25348fd438d19dd2a17942088f20b4cad48f
Andrew Fister <temposs@gmail.com>
2012-03-18 02:08:07 -0700

    dynamic menu transition

--- event/urls.py
+++ event/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('gallevent.event.views',
     (r'^post/$', 'post_event'),
     (r'^edit/$', 'edit_event'),
     (r'^show/$', 'show_events'),
+    (r'^lineup/$', 'show_lineup'),
     (r'^manage/$', 'manage_events'),
 )
--- event/views.py
+++ event/views.py
@@ -7,7 +7,10 @@ def edit_event(request):
     return render_to_response('post-event.html', {'edit': True})
 
 def show_events(request):
-    return render_to_response('your-posts.html')
+    return render_to_response('your-posts.html', {'selected_page': 'your-posts'})
+
+def show_lineup(request):
+    return render_to_response('your-posts.html', {'selected_page': 'your-events'})
 
 def manage_events(request):
     return render_to_response('your-posts-manage.html')
