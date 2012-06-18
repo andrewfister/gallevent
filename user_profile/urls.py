1980c8e685af82301c3ffb3082d3ad8b41889e39
Jessica Greenwalt <jgreenwalt@Jessicas-MacBook-Air-2.local>
2012-06-17 17:03:39 -0700

    overwriting urls.py with local copy

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,4 +4,5 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns('gallevent.user_profile.views',
     (r'^show$', 'show_profile'),
     (r'^event_prefs$', direct_to_template, {'template': 'your-profile-ep.html'}),
+    (r'^profile_form$', direct_to_template, {'template': 'your-profile-form.html'}),
 )
