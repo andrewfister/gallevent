fde2f186480273c85a66dfd8e3c8f66fb6613660
Andrew Fister <temposs@gmail.com>
2012-07-15 16:10:41 -0700

    reorganize int a view

---
+++ www/js/views/post-event-view.js
@@ -0,0 +1,62 @@
+var PostEventView = Backbone.View.extend({
+    id: "post-event",
+    
+    render: function() {
+        // Watermark all input tags:
+	    $("input[type='text']").each(function(index, input) {  // Find each input tag
+	        input = $(input);              // each gives you a raw HTML element; wrap it back in input
+	        if (input.attr('title')) { // if that element has a watermark attribute
+		        input.watermark(input.attr('title')); // Use that attribute as the watermark
+		    }
+	    });
+	
+	    // Make formOptional divs expand & collapse:
+	    $(".formOptional").hide();
+	    $(".formOptional").each(function(index, elem) {
+		    var visible = false;
+		    $(elem).siblings(".formOptionalToggle").change(function(event) {
+			    var content = $(event.target).siblings(".formOptional");
+			    if (visible) {
+				    content.slideUp();
+			    } else {
+				    content.slideDown();
+			    }
+			    visible = !visible;
+		    });
+	    });
+        
+        return this;
+    },
+    
+    codeAddress: function(address, infoWindow, map, geocoder) {
+
+        var image = '/static/img/pin-map-dining.png';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: infoWindow
+        });
+
+        geocoder.geocode( { 'address': address}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                map.setCenter(results[0].geometry.location);
+                var marker = new google.maps.Marker({
+                    map: map,
+                    draggable:true,
+                    position: results[0].geometry.location,
+                    icon: image,
+                    title : address,
+                    animation: google.maps.Animation.DROP
+                });
+
+                google.maps.event.addListener(marker, 'click', function() {
+                    infowindow.close();
+                    infowindow.open(map,marker);
+                    last_marker = marker;
+                });
+
+            } else {
+                alert("Geocode was not successful for the following reason: " + status); 
+            }
+        });
+    },
+});
