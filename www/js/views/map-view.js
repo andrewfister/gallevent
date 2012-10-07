045f85492fc093776cf03bd8b2cdbe8156893b7f
Andrew Fister <temposs@gmail.com>
2012-10-07 14:43:15 -0700

    center location on user location if available

--- www/js/views/map-view.js
+++ www/js/views/map-view.js
@@ -19,10 +19,12 @@ var MapView = Backbone.View.extend({
     
     markers: new Array(),
     
+    location: new google.maps.LatLng(37.88397, -122.2644),
+    
     render: function() {
         google.maps.event.addDomListener(window, 'load', function() {
             var myOptions = {
-                center: new google.maps.LatLng(37.88397, -122.2644), 
+                center: this.location,
                 zoom: 9,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
@@ -30,7 +32,10 @@ var MapView = Backbone.View.extend({
             this.map = new google.maps.Map($("#map_canvas").get(0),
                 myOptions);
 
+            navigator.geolocation.getCurrentPosition(this.foundUserLocation.bind(this),this.noUserLocation.bind(this),{timeout:10000});
+
             google.maps.event.addListener(this.map, 'tilesloaded', function() {
+                this.map.setCenter(this.location);
                 this.setAllMarkers();
                 google.maps.event.clearListeners(this.map, 'tilesloaded');
             }.bind(this));
@@ -58,7 +63,6 @@ var MapView = Backbone.View.extend({
         });
 
         var location = new google.maps.LatLng(latitude, longitude);
-        this.map.setCenter(location);
         
         var marker = new google.maps.Marker({
             map: this.map,
@@ -86,5 +90,17 @@ var MapView = Backbone.View.extend({
     destroyMarker: function(index) {
         this.markers[index].setMap(null);
         this.markers.splice(index, 1);
+    },
+    
+    foundUserLocation: function(position) {
+        this.location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        
+        if (this.map != null)
+        {
+            this.map.setCenter(this.location);
+        }
+    },
+    
+    noUserLocation: function() {
     }
 });
