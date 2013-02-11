693fc82afeae2d3b227cc3e555e06ba4ba863257
Andrew Fister <temposs@gmail.com>
2013-02-10 18:38:47 -0800

    selecting date ranges prettily

--- www/js/views/header-view.js
+++ www/js/views/header-view.js
@@ -1,18 +1,18 @@
 var HeaderView = Backbone.View.extend({
     id: "front-page",
     render: function() {
-        $( "#date1" ).datepicker({ dateFormat: "mm/dd/yyyy", onSelect: this.changeDate });
+        $( "#date1" ).datepicker({ dateFormat: "mm/dd/yy", onSelect: this.changeDate, firstDay: 1, beforeShowDay: this.styleDateRange, minDate: new Date() });
         $("#time-span").change(this.changeDate);
 		$("#header").slideToggle(600);
 		
 		if ($("#date1").attr('value').length == 0)
 		{
 		    var today = new Date();
-		    var dd = ('0'+(today.getDate())).slice(-2);
+		    /*var dd = ('0'+(today.getDate())).slice(-2);
 		    var mm = ('0'+(today.getMonth()+1)).slice(-2);
 		    var yyyy = today.getFullYear();
-		    var formatToday = mm + '/' + dd + '/' + yyyy;
-		    $("#date1").attr('value', formatToday);
+		    var formatToday = mm + '/' + dd + '/' + yyyy;*/
+		    $("#date1").attr('value', $.datepicker.formatDate("mm/dd/yy", today));
 		}
 		
 		if ($("#date2").attr('value').length == 0)
@@ -22,20 +22,51 @@ var HeaderView = Backbone.View.extend({
     },
     
     changeDate: function() {
-        if ($("time-span").attr('value') == "day_of")
+        if ($("#time-span").attr('value') == "day_of")
         {
             $("#date2").attr('value', $( "#date1" ).attr('value'));
         }
-        else if ($("time-span").attr('value') == "week_of")
+        else if ($("#time-span").attr('value') == "week_of")
         {
-            var date = new Date($( "#date1" ).attr('value'));
-            var dayOfWeek = (date.getDay() + 1) % 7;
+            var date = $.datepicker.parseDate('mm/dd/yy', $( "#date1" ).attr('value'));
+            var dayOfWeek = (date.getDay() - 1) % 7;
+            date.setDate(date.getDate() - dayOfWeek);
+            var date2 = new Date();
+            date2.setDate(date.getDate() + 6);
             
+            $("#date1").attr('value', $.datepicker.formatDate('mm/dd/yy', date));
+            $("#date2").attr('value', $.datepicker.formatDate('mm/dd/yy', date2));
         }
-        else if ($("time-span").attr('value') == "weekend_of")
+        else if ($("#time-span").attr('value') == "weekend_of")
         {
-            var date = new Date($( "#date1" ).attr('value'));
-            var dayOfWeek = (date.getDay() + 1) % 7;
+            var date = $.datepicker.parseDate('mm/dd/yy', $( "#date1" ).attr('value'));
+            var dayOfWeek = (date.getDay() - 1) % 7;
+            date.setDate(date.getDate() - dayOfWeek + 4);
+            var date2 = new Date();
+            date2.setDate(date.getDate() + 2);
+            
+            $("#date1").attr('value', $.datepicker.formatDate('mm/dd/yy', date));
+            $("#date2").attr('value', $.datepicker.formatDate('mm/dd/yy', date2));
         }
+    },
+    
+    styleDateRange: function(date) {
+        var now = new Date();
+        var today = new Date(now.getFullYear(), now.getMonth(), now.getDay());
+        var selectable = date.valueOf() >= today.valueOf();
+        var style = "";
+        var date1 = $.datepicker.parseDate('mm/dd/yy', $( "#date1" ).attr('value'));
+        var date2 = $.datepicker.parseDate('mm/dd/yy', $( "#date2" ).attr('value'));
+        
+        if (date.valueOf() >= date1.valueOf() && date.valueOf() <= date2.valueOf())
+        {
+            style = "ui-datepicker-today";
+        }
+        else if (date.valueOf() == today.valueOf())
+        {
+            style = "";
+        }
+        
+        return [selectable, style];
     }
 });
