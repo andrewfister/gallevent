4a9bfe941bf4b9cf5e8111ebc5b38ed9ee115031
Andrew Fister <temposs@gmail.com>
2013-09-15 14:55:24 -0700

    local static root

--- gallevent/settings.py
+++ gallevent/settings.py
@@ -59,7 +59,7 @@ USE_I18N = True
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/andrew/Projects/gallevent/static/'
+STATIC_ROOT = local_settings.STATIC_ROOT_LOCAL
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
