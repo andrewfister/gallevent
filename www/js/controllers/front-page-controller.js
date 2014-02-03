48936f067cc234c48209b43f83cd68931256aeda
Andrew Fister <temposs@gmail.com>
2014-02-03 01:20:42 -0800

    communicate every search

--- www/js/controllers/front-page-controller.js
+++ www/js/controllers/front-page-controller.js
@@ -1,5 +1,6 @@
 // on dom load
 $(document).ready(function() {
+    window.dispatcher = _.clone(Backbone.Events);
     window.events = new EventSearchCollection();
 
     window.searchView = new SearchView({
