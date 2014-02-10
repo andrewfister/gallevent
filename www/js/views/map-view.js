03271c79965c0d73e594603c408517908db6dbfb
stchourou <stchourou@ucdavis.edu>
2014-02-09 17:29:24 -0800

    commit cookie changes

--- www/js/views/map-view.js
+++ www/js/views/map-view.js
@@ -6,16 +6,13 @@ var MapView = Backbone.View.extend({
         this.hasVisibleMarkers = false;
     
         this.collection.on("reset", function(events) {
-<<<<<<< HEAD
 		if(this.mapLoaded){
 		    this.loadMarkers();
 		}
-=======
             this.eventsLoaded = true;
             if (!this.markersLoaded && this.mapLoaded) {
                 this.loadMarkers();
             }
->>>>>>> 48936f067cc234c48209b43f83cd68931256aeda
         }, this);
 
         this.collection.on("remove", function(evt, collection, options) {
@@ -90,7 +87,7 @@ var MapView = Backbone.View.extend({
     },
     
     getCurrentPosition: function() {
-        navigator.geolocation.getCurrentPosition(this.foundUserLocation.bind(this),this.noUserLocation.bind(this),{timeout:5000});
+	    navigator.geolocation.getCurrentPosition(this.foundUserLocation.bind(this),this.noUserLocation.bind(this),{timeout:5000});
     },
 
     loadMap: function() {
@@ -321,35 +318,61 @@ var MapView = Backbone.View.extend({
 
         var userLatitude = parseFloat(this.userLocation.lat());
         var userLongitude = parseFloat(this.userLocation.lng());
+	      
+	localStorage.setItem('user-latitude', userLatitude);
+	localStorage.setItem('user-longitude', userLongitude);
 
         //Set user location data on page and in cookie
-        if ($('#user-latitude').length)
+	/*
+	if ($('#user-latitude').length)
         {
             $('#user-latitude').html(userLatitude);
-            $.cookie('user-latitude', userLatitude, { expires: 1, path: '/' });
+            $.cookie('user-latitude', userLatitude, { expires: 2.7e-7, path: '/' });
         }
         if ($('#user-longitude').length)
         {
             $('#user-longitude').html(userLongitude);
-            $.cookie('user-longitude', userLongitude, { expires: 1, path: '/' });
+            $.cookie('user-longitude', userLongitude, { expires: 2.7e-7, path: '/' });
         }
-        
+        */
+
+	var cookieLat = $.cookie('user-latitude');
+        var cookieLon = $.cookie('user-longitude');
+	if (cookieLat == null && cookieLon == null){
+	    $.cookie('user-latitude', userLatitude, { expires: 1, path: '/' });
+	    $.cookie('user-longitude', userLongitude, { expires: 1, path: '/' });
+	
+	    alert( cookieLat +  '   '  + cookieLon);
+	}
+
         this.setMapLocation(true);
-        
+ 
         this.loadMap();
     },
 
     noUserLocation: function() {
+
         var userLatitude = $.cookie('user-latitude');
         var userLongitude = $.cookie('user-longitude');
 
+       	alert('no location:' + userLatitude +  '   '  +  userLongitude);
+	//try from cookies
         if (userLatitude != null && userLongitude != null)
         {
             this.userLocation = new google.maps.LatLng(userLatitude, userLongitude);
             this.setMapLocation(true);
         }
         else {
-            this.setMapLocation(false);
+	    //try from local storage
+	    userLatitude =localStorage.getItem('user-latitude');
+	    userLongitude = localStorage.getItem('user-longitude');
+	    if (userLatitude != null && userLongitude != null)
+		{
+		    this.userLocation = new google.maps.LatLng(userLatitude, userLongitude);
+		    this.setMapLocation(true);
+		}
+	    else
+		this.setMapLocation(false);
         }
         
         this.loadMap();
