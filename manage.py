7511b461b86198a402fc006292c1ff4fcdc215ab
Andrew Fister <temposs@gmail.com>
2014-04-27 15:28:13 -0700

    add requirements and update manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
-from django.core.management import execute_manager
-from gallevent import settings
+import os
+import sys
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gallevent.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
