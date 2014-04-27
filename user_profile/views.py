53a6702bb43f691207fee0aa11327453fedc7d4e
Andrew Fister <temposs@gmail.com>
2014-04-27 16:03:20 -0700

    new version of django

--- user_profile/views.py
+++ user_profile/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.models import User
 
-#from djangbone.views import BackboneAPIView
-
 def show_profile(request):
     return render_to_response('profile.html', {
     }, context_instance=RequestContext(request))
